3f4f52929d0b20e7fc623523d5a732eebf813556
Richard Wilkes <wilkes@me.com>
2019-01-11 11:19:08 -0800

    Adapt to changes in webapp

--- internal/ui/start.go
+++ internal/ui/start.go
@@ -17,11 +17,19 @@ func Start(args *cef.MainArgs, s *server.Server) {
 		wnd, err := webapp.NewWindow(webapp.StdWindowMask, webapp.MainDisplay().UsableBounds, fmt.Sprintf("%s v%s", cmdline.AppName, cmdline.AppVersion), fmt.Sprintf("http://127.0.0.1:%d", s.Port()))
 		jot.FatalIfErr(err)
 		if bar, global, first := webapp.MenuBarForWindow(wnd); !global || first {
-			stdmenu.FillMenuBar(bar, func() { fmt.Println("About menu item selected") }, func() { fmt.Println("Preferences menu item selected") })
+			stdmenu.FillMenuBar(bar, aboutHandler, prefsHandler, true)
 		}
 		wnd.ToFront()
 	}
 	// Start only returns on error
 	jot.FatalIfErr(webapp.Start(args, nil, nil))
 	atexit.Exit(0)
+}
+
+func aboutHandler() {
+	fmt.Println("About menu item selected")
+}
+
+func prefsHandler() {
+	fmt.Println("Preferences menu item selected")
 }
